9f771736c9bee9f82e8fce17019a656b1f9c771c
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-18 20:22:55 +0530

    Update useToggle.js

--- src/components/useToggle.js
+++ src/components/useToggle.js
@@ -1,7 +1,14 @@
 import React,{useCallback,useState} from 'react'
 
-const useToggle = () => {
-   
+
+const useToggle = (initialState = false) => {
+  const [state, setState] = useState(initialState);
+
+  const toggle = useCallback(() => {
+    setState(prevState => !prevState);
+  }, []);
+
+  return [state, toggle];
   
-  }
-  export default useToggle;
+}
+export default useToggle;
